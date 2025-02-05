20d3c0aa162f83a3b135c86d881725cb37f775ea
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2025-02-05 15:56:20 +0300

    fix tailwind build in prod

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RailsStock
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.assets.css_compressor = nil
+    config.assets.compile = true
+    config.assets.precompile += %w( application.tailwind.css )
 
     # Configuration for the application, engines, and railties goes here.
     #
